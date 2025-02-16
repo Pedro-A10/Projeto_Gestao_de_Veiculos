0ab990d9287a0836d8181f43a3b18d1aa67b8446
Pedro A. <pedroaugusto732@gmail.com>
2025-02-15 22:24:11 -0300

    Update Accessory.java

--- src/model/entities/vehicles/Accessory.java
+++ src/model/entities/vehicles/Accessory.java
@@ -1,4 +1,29 @@
-package model.entities;
+package model.entities.vehicles;
 
 public class Accessory {
+
+    private String name;
+    private double price;
+
+    public Accessory() {
+    }
+
+    public Accessory( String name, double price ) {
+        if (name == null) {
+            throw new IllegalArgumentException("O nome não pode ser nulo.");
+        }
+        if (price <= 0) {
+            throw new IllegalArgumentException("O preço não pode ser menor.");
+        }
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
 }

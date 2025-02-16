286490e622ac5c5f233013a3014ce86fba381553
Pedro A. <pedroaugusto732@gmail.com>
2025-02-15 22:22:31 -0300

    Update Car.java

--- src/model/entities/vehicles/car/Car.java
+++ src/model/entities/vehicles/car/Car.java
@@ -1,4 +1,33 @@
 package model.entities.vehicles.car;
 
-public class Car {
+import model.entities.vehicles.Accessory;
+import model.entities.vehicles.Engine;
+import model.entities.vehicles.Vehicle;
+
+public class Car extends Vehicle {
+
+    private int numDoors;
+
+    public Car() {
+        super();
+    }
+
+    public Car( String brand, String model, int year, Engine engine, int numDoors ) {
+        super(brand, model, year, engine);
+        if (numDoors <= 0) {
+            throw new IllegalArgumentException("O Número de portas não pode ser nulo.");
+        }
+        this.numDoors = numDoors;
+    }
+
+    @Override
+    public void displayDetails() {
+        System.out.println();
+        System.out.println("Carro: " + getBrand() + " " + getModel() + " (" + getYear() +")" );
+        System.out.println("Numero de portas: " + numDoors);
+        System.out.println("Motor: " + getEngine().getPower() + " cavalos," + getEngine().getFuelType() );
+        for (Accessory accessory : getAccessories()) {
+            System.out.println(" - " + accessory.getName() + " (R$" + accessory.getPrice() + ")");
+        }
+    }
 }

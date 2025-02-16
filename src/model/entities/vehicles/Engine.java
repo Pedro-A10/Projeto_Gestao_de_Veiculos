8f99c2caf25c04ccca496faa022b49f71add0131
Pedro A. <pedroaugusto732@gmail.com>
2025-02-15 22:24:31 -0300

    Update Engine.java

--- src/model/entities/vehicles/Engine.java
+++ src/model/entities/vehicles/Engine.java
@@ -1,4 +1,30 @@
-package model.entities;
+package model.entities.vehicles;
 
 public class Engine {
+
+    private int power;
+    private String fuelType;
+
+    public Engine(){
+    }
+
+    public Engine( int power, String fuelType ) {
+        if (power <= 0) {
+            throw new IllegalArgumentException("Potência tem que ser positiva.");
+        }
+        if (fuelType == null || fuelType.trim().isEmpty()) {
+            throw new IllegalArgumentException("O tipo de combustível não pode ser nulo ou vazio.");
+        }
+        this.power = power;
+        this.fuelType = fuelType;
+    }
+
+
+    public int getPower() {
+        return power;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
 }
